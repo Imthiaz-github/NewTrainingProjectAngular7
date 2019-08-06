c89f6021a263f74aecf91e404c6501c6825e948d
Imthiaz Shaik <imthiaz.shaik@greenstone.com.au>
2019-08-06 13:52:52 +1000

    Added the Angular 7 project
    Added the Angular 7 project

---
+++ Test/WebAPI/WebAPI.Tests/Controllers/TrainingControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Http;
+using System.Web.Http.Results;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers.Tests
+{
+    [TestClass()]
+    public class TrainingControllerTest
+    {
+        [TestMethod()]
+        public void PutTrainingPostTraining()
+        {
+            var controller = new TrainingController();
+            Training training = new Training
+            {
+                TrainingName = "Test Training",  
+                StartDate = System.DateTime.Now,
+                EndDate = System.DateTime.Now
+            };
+            // Act  
+            IHttpActionResult actionResult = controller.PostTraining(training);
+            var createdResult = actionResult as CreatedAtRouteNegotiatedContentResult<Training>;
+            // Assert  
+            Assert.IsNotNull(createdResult);
+            Assert.AreEqual("DefaultApi", createdResult.RouteName);
+            Assert.IsNotNull(createdResult.RouteValues["id"]);
+        }
+    }
+}
---
+++ Test/WebAPI/WebAPI/Controllers/TrainingController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class TrainingController : ApiController
+    {
+        private DBModel db = new DBModel();
+
+        // GET: api/Training
+        public IQueryable<Training> GetTrainings()
+        {
+            return db.Trainings;
+        }
+
+        // PUT: api/Training/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTraining(int id, Training training)
+        {
+            if (id != training.TrainingID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(training).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TrainingExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Training
+        [ResponseType(typeof(Training))]
+        public IHttpActionResult PostTraining(Training training)
+        {
+            db.Trainings.Add(training);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = training.TrainingID }, training);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TrainingExists(int id)
+        {
+            return db.Trainings.Count(e => e.TrainingID == id) > 0;
+        }
+    }
+}
